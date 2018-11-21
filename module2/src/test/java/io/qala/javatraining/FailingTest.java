ef8b96496323b76154431756951048e362788cd1
Yana <9301YanaGuseva@gmail.com>
2018-11-21 03:02:58 +0300

    fix test

--- module2/src/test/java/io/qala/javatraining/FailingTest.java
+++ module2/src/test/java/io/qala/javatraining/FailingTest.java
@@ -4,12 +4,14 @@ package io.qala.javatraining;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
+import java.io.InputStream;
 
 import static org.junit.Assert.assertEquals;
 
 public class FailingTest {
     @Test public void strangeFailure() throws Exception {
-        String file = ClassLoader.getSystemResource("myresource.txt").getFile();
-        assertEquals(new FileInputStream(file).read(), 49);
+//        String file = ClassLoader.getSystemResource("myresource.txt").getFile();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("myresource.txt");
+        assertEquals(inputStream.read(), 49);
     }
 }
