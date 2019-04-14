107d5330da03a7271cc508294551f97847ccb462
Yana <9301YanaGuseva@gmail.com>
2019-04-14 18:04:44 +0300

    fix test

--- src/test/java/io/qala/javatraining/dao/HibernateDogDaoTest.java
+++ src/test/java/io/qala/javatraining/dao/HibernateDogDaoTest.java
@@ -2,6 +2,7 @@ package io.qala.javatraining.dao;
 
 import io.qala.javatraining.HibernateDaoTest;
 import io.qala.javatraining.domain.Dog;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
 import org.testng.annotations.Test;
@@ -12,12 +13,16 @@ import static io.qala.datagen.RandomShortApi.english;
 
 @Test @HibernateDaoTest
 public class HibernateDogDaoTest extends AbstractTransactionalTestNGSpringContextTests {
+    @Autowired
+    SessionFactory sessionFactory;
 
     @Test(expectedExceptions = ConstraintViolationException.class, expectedExceptionsMessageRegExp = "Validation failed.*")
     public void validationIsInvoked_byOrm_duringSaving() {
         Dog original = Dog.random();
         original.setName(english(101)/*fails a validation rule*/);
         dao.createDog(original);
+        sessionFactory.getCurrentSession().flush();
+        sessionFactory.getCurrentSession().clear();
     }
 
     @Autowired private HibernateDogDao dao;
